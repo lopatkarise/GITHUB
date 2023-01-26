8e39ca2d801ddf664b2d527f16c9371898f4fb9b
lopatkarise <lopatkarise@gmail.com>
2023-01-26 20:20:33 +0000

    Added fill

--- GITHUB/Source.cpp
+++ GITHUB/Source.cpp
@@ -3,8 +3,14 @@ using namespace std;
 int func(int x) {
 	return x + 10;
 }
+void fill(int arr[], int size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = size;
+	}
+}
+int arr[10];
 int main() {
-
-
+	fill(arr, 10);
 	cout << "Hello world!" << func(20);
 }
